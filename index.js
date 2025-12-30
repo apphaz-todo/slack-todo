3e58c1dd9502abdae829f514382e0880035f70a0
apphaz-todo <slack-todo@apphaz.com>
2025-12-29 21:42:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const app = new App({
 ------------------------------ */
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_ANON_KEY
 );
 
 /* -----------------------------
